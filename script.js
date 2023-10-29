1771a7880c9e89f938616542818cd13310ea5e8f
adityabarodiya <adi25kht@gmail.com>
2023-10-29 21:00:19 +0530

    completed project

--- script.js
+++ script.js
@@ -6,13 +6,36 @@ function addTask(){
         let li = document.createElement('li');
         li.innerHTML = input.value;
         list.appendChild(li)
-        input.value = '';
-
+        
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
     }
     else{
         alert('You must write something!!')
     }
+    input.value = '';
+    saveData()
 }
 
+list.addEventListener("click", (e) => {
+    if (e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData()
+    }
+},false)
 
 
+function saveData (){
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function showTask(){
+    list.innerHTML = localStorage.getItem("data")
+
+}
+showTask()
