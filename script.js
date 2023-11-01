a519641768c5d6455cbc03260d9f47aa506a5331
adityabarodiya <adi25kht@gmail.com>
2023-11-01 22:11:58 +0530

    updated

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const list = document.getElementById('list-container');
 
 function addTask(){
     if(input.value !== ''){
+        
         let li = document.createElement('li');
         li.innerHTML = input.value;
         list.appendChild(li)
